59dc018a5b5c0dafc8d5576b8112f3b0b7552915
Sloot04 <suyaislootmans@gmail.com>
2021-06-18 10:59:11 -0300

    Animación del disco

--- lib/src/pages/music_player_page.dart
+++ lib/src/pages/music_player_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
+
+import 'package:provider/provider.dart';
+import 'package:music_player/src/models/audioplayer_model.dart';
+
 import 'package:music_player/src/widgets/custom_appbar.dart';
 import 'package:music_player/src/helpers/helpers.dart';
 
@@ -117,12 +122,17 @@ class _TituloPlayState extends State<TituloPlay>
             elevation: 0.0,
             highlightElevation: 0.0,
             onPressed: () {
+
+              final audioPlayerModel = Provider.of<AudioPlayerModel>(context, listen: false);
+
               if (this.isPlaying) {
                 playAnimation.reverse();
                 this.isPlaying = false;
+                audioPlayerModel.controller.stop();
               } else {
                 playAnimation.forward();
                 this.isPlaying = true;
+                audioPlayerModel.controller.repeat();
               }
             },
             backgroundColor: Color(0xffF8CB51),
@@ -192,6 +202,8 @@ class BarraProgreso extends StatelessWidget {
 class ImagenDisco extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    final audioPlayerModel = Provider.of<AudioPlayerModel>(context);
     return Container(
       padding: EdgeInsets.all(20.0),
       width: 250,
@@ -206,8 +218,14 @@ class ImagenDisco extends StatelessWidget {
         child: Stack(
           alignment: Alignment.center,
           children: [
-            Image(
-              image: AssetImage('assets/aurora.jpg'),
+            SpinPerfect(
+              duration: Duration(seconds: 10),
+              infinite: true,
+              manualTrigger: true,
+              controller: (animationController)=> audioPlayerModel.controller = animationController,
+              child: Image(
+                image: AssetImage('assets/aurora.jpg'),
+              ),
             ),
             Container(
               width: 25,
